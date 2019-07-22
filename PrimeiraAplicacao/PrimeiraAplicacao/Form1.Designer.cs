da662b8c5565cbc7701b606216cd943233901f89
railander jose <railandergama@gmail.com>
2019-07-22 13:57:39 -0300

    Atividade curso de C# - Novato a profissional

---
+++ PrimeiraAplicacao/PrimeiraAplicacao/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PrimeiraAplicacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.btn_executar = new System.Windows.Forms.Button();
+            this.cx_texto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(345, 243);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(83, 35);
+            this.btn_sair.TabIndex = 0;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // btn_executar
+            // 
+            this.btn_executar.Location = new System.Drawing.Point(236, 243);
+            this.btn_executar.Name = "btn_executar";
+            this.btn_executar.Size = new System.Drawing.Size(83, 35);
+            this.btn_executar.TabIndex = 1;
+            this.btn_executar.Text = "Executar";
+            this.btn_executar.UseVisualStyleBackColor = true;
+            this.btn_executar.Click += new System.EventHandler(this.btn_executar_Click);
+            // 
+            // cx_texto
+            // 
+            this.cx_texto.Location = new System.Drawing.Point(3, 131);
+            this.cx_texto.Name = "cx_texto";
+            this.cx_texto.Size = new System.Drawing.Size(572, 49);
+            this.cx_texto.TabIndex = 2;
+            this.cx_texto.Text = "Texto aqui";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 290);
+            this.Controls.Add(this.cx_texto);
+            this.Controls.Add(this.btn_executar);
+            this.Controls.Add(this.btn_sair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Button btn_executar;
+        private System.Windows.Forms.Label cx_texto;
+    }
+}
+
---
+++ PrimeiraAplicacao/PrimeiraAplicacao/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrimeiraAplicacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btn_executar_Click(object sender, EventArgs e)
+        {
+            String var = "Railander José";
+            cx_texto.Text = var;
+            /*int valor = 35;
+            cx_texto.Text = valor.ToString();
+            */            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
